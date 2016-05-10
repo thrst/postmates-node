774655f5a304c7398449c614a6576a98ecde8298
Preston Van Loon <preston@machinepowered.com>
2016-05-10 17:08:32 -0500

    Added delivery zones, tests, travis, readme update

--- test/index.js
+++ test/index.js
@@ -1,9 +1,33 @@
+'use strict';
 
-/**
- * Test dependencies.
- */
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var request = require('superagent');
 
-var Mocha = require('mocha');
-var fs = require('fs');
-var path = require('path');
+var Postmates = require('../lib');
 
+var customerId = 'customerId';
+var apiKey = 'apiKey';
+var postmates = new Postmates(customerId, apiKey);
+
+describe('Postmates', function () {
+  beforeEach(function() {
+    sinon.spy(request, 'get');
+    sinon.stub(request.Request.prototype, 'end').yields(null, {});
+  });
+
+  afterEach(function() {
+    request.get.restore();
+    request.Request.prototype.end.restore();
+  });
+
+  describe('Delivery zones', function () {
+    it('should GET "delivery_zones"', function (done) {
+      postmates.zones(function (err, zones) {
+        expect(request.get).to.be.called;
+        expect(request.get.firstCall.args[0]).to.match(/delivery_zones$/);
+        done();
+      });
+    });
+  });
+});
